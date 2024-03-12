4ff4459e951972eeb10607b8ffa329ab86df2373
gs125 <gs214gs@gmail.com>
2024-03-12 12:15:29 +0100

    Dodaj pliki projektów.

---
+++ AppointmentMaker/Controllers/AppointmentController.cs
@@ -0,0 +1,25 @@
+﻿using AppointmentMaker.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppointmentMaker.Controllers
+{
+    public class AppointmentController : Controller
+    {
+        static List<AppointmentModel> appointments = new List<AppointmentModel>();
+        public IActionResult Index()
+        {
+            return View(appointments);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Details(AppointmentModel appointment) 
+        {
+            appointments.Add(appointment);
+            return View(appointment);
+        }
+    }
+}
---
+++ AppointmentMaker/Models/AppointmentModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace AppointmentMaker.Models
+{
+    public class AppointmentModel
+    {
+        [StringLength(20, MinimumLength =4)]
+        [DisplayName("Patient's full name")]
+        public required string PatientName { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayName("Appointment Request Date")]
+        public DateTime AppointmentDate { get; set; }
+        [DataType(DataType.Currency)]
+        [Range(90000, maximum:Int32.MaxValue)]
+        [DisplayName("Patient's Approximate Net Worth")]
+        public decimal PatientNetWorth { get; set; }
+        [DisplayName("Patient's Doctor's Last Name")]
+        public required string DoctorName { get; set; }
+        [Range(6,10)]
+        [DisplayName("Patient's Perceived Level of Pain (1 low to 10 high)")]
+        public int PainLevel { get; set; }
+    }
+}
